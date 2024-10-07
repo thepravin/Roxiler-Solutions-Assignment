38415c7c5b44774bc527a11ea43706079f785eec
Pravin Nalawade <thepravin28@gmail.com>
2024-10-07 23:42:27 +0530

    gitignor update

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -15,7 +15,7 @@ const App = () => {
     useEffect(() => {
         const apiCall = async (month) => {
             try {
-                const response = await fetch(`http://localhost:5000/api/transactions/combined-data?month=${month}`);
+                const response = await fetch(`${import.meta.env.VITE_URL}/combined-data?month=${month}`);
                 const result = await response.json();
                 
                 // Set state with respective data
